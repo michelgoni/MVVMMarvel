4c83bd473ec5f7078a1a57f9cc0011b99834944e
Michel Goñi <michelgoni2@gmail.com>
2017-12-26 16:03:20 +0100

    Removing pods

--- Podfile
+++ Podfile
@@ -6,15 +6,12 @@ target 'MVVMarvel' do
   use_frameworks!
 
   # Pods for MVVMarvel
-  pod 'RxSwift',    '~> 4.0'
-  pod 'RxCocoa',    '~> 4.0'
-  pod 'RxAlamofire'
-  pod 'RxDataSources', '~> 3.0'
+
 
   target 'MVVMarvelTests' do
     inherit! :search_paths
     # Pods for testing
-     pod 'RxSwift',    '~> 4.0'
+    
   end
 
   target 'MVVMarvelUITests' do
--- Pods/Target Support Files/Pods-MVVMarvel/Pods-MVVMarvel-frameworks.sh
+++ Pods/Target Support Files/Pods-MVVMarvel/Pods-MVVMarvel-frameworks.sh
@@ -100,23 +100,6 @@ strip_invalid_archs() {
   fi
 }
 
-
-if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Differentiator/Differentiator.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/RxAlamofire/RxAlamofire.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/RxDataSources/RxDataSources.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
-fi
-if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Differentiator/Differentiator.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/RxAlamofire/RxAlamofire.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/RxDataSources/RxDataSources.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
-fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
 fi
